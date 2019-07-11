def211b246eab1a763aa13d0a723a4e76a846f06
virus4297 <nammanewsvirus4297@users.noreply.github.com>
2019-07-11 20:32:27 +0530

    sticky

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 /*JS FOR STICKY TOP NAVIGATION BAR */
 	// When the user scrolls the page, execute myFunction 
-	// window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
 	// // Get the topnav
-	// var topnav = document.getElementById("topnav");
+var topnav = document.getElementById("topnav");
 
 	// // Get the offset position of the topnav
-	// var sticky = topnav.offsetTop;
+var sticky = topnav.offsetTop;
+
+var	bot = document.getElementById("botnav");
+var botstick = bot.offsetTop;
 
 	// // Add the sticky class to the topnav when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	// function myFunction() {
-	//   if (window.pageYOffset >= sticky) {
-	//     topnav.classList.add("sticky")
-	//   } else {
-	//     topnav.classList.remove("sticky");
-	//   }
-	// } 
+function myFunction() {
+	  if (botstick >= sticky) {
+	    topnav.add("position:static;")
+	  } else {
+	    topnav.classList.remove("sticky");
+	  }
+	} 
 
 
 
