8ae68e8f0589236f3223617fdf71cc3c03389e12
Pawan.V <42887444+Pawan2804@users.noreply.github.com>
2019-07-24 18:20:05 +0530

    Getting Data

---
+++ trial/trial/display.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="display.aspx.cs" Inherits="trial.WebForm1" %>
+
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+      
+        <div>
+            <asp:Label ID="Get1" runat="server" Text="Label"></asp:Label>
+        </div>   
+            
+        
+    </form>
+</body>
+</html>
---
+++ trial/trial/display.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace trial
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string Conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Pawan\Desktop\work\BTCH98\trial\trial\App_Data\Database1.mdf;Integrated Security=True";
+            SqlConnection con =new SqlConnection(Conn);
+            con.Open();
+            SqlCommand query1 = new SqlCommand("Select * from Book");
+            query1.CommandType = System.Data.CommandType.Text;
+            query1.Connection = con;
+
+            string temp = "";
+            SqlDataReader reader = query1.ExecuteReader();
+            while (reader.Read())
+            {
+                temp += "<table>";
+                temp += reader["name"].ToString();
+                temp +=  "<br>";
+                temp += reader["contact"].ToString();
+                temp += reader["email"].ToString();
+                temp += reader["book_name"].ToString();
+                temp += reader["description"].ToString();
+                temp += "<br>";
+                temp += "</table>";
+            }
+            Get1.Text = temp;
+            con.Close();
+        }
+
+    }
+}
